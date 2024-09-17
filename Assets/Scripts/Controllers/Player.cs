0cc6a99af0cb38465eedd4f7ae9e27ffc0e9d77b
PeytonLilley <148352069+PeytonLilley@users.noreply.github.com>
2024-09-17 14:09:33 -0400

    Update Player.cs
    Implemented PlayerMovement method

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,31 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public float speed = 0.05f;   // added a public speed variable to be able to change the speed from the inspector
+
     void Update()
     {
+        PlayerMovement();
+    }
 
+    void PlayerMovement()   // added an if statement for each direction, now it moves 1 in each direction depending on the key pressed
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + (Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (Vector3.up * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + (Vector3.down * speed);   // multiplied by speed variable to change speed of movement
+        }
     }
 
 }
